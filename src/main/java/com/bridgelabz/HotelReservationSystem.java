9a352cb286364b5b252d1cb28199119b07e4e0f0
vamsikrish-A <krishnavamsi165@gmail.com>
2021-09-23 08:32:24 +0530

    Welcome message

---
+++ src/main/java/com/bridgelabz/HotelReservationSystem.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+
+/*
+* @author: VamsiKrishna A
+*
+* @purpose: workshop on Hotel Reservation System
+*
+* @date: 23-09-2021
+* */
+public class HotelReservationSystem {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Hotel Reservation Program.");
+    }
+}
